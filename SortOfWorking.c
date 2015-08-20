5a441e2976b2f912daad0fcdcc4bceeaac087d5c
CopyPaste-CP <kaayathire.arumugan14@gmail.com>
2015-08-20 18:57:22 +0800

    Create SortOfWorking.c

---
+++ SortOfWorking.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	char returnData[64];
+	char args[50][50];
+	char *p;
+	int a;
+	
+	fp = popen("/sbin/ifconfig eth0", "r");
+	while (fgets(returnData, 64, fp) != NULL)
+	{
+		
+		p=strtok(returnData, " ");
+		do
+		{
+			strcpy(args[a],p);
+			a=a+1;
+			printf("%s\n",args[7]);	
+			p=strtok(NULL," ");
+		}
+		while(p!=NULL && a!=8);
+	}
+pclose(fp);
+}
